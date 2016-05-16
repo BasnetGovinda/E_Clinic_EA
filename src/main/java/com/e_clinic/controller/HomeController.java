2f10229447e349b45753928e907d97ef4d1dc32b
zeelosoft <zeelosoft@gmail.com>
2016-05-16 00:43:50 -0500

    first

--- src/main/java/com/e_clinic/controller/HomeController.java
+++ src/main/java/com/e_clinic/controller/HomeController.java
@@ -1,21 +1,51 @@
 package com.e_clinic.controller;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
 
 	@RequestMapping("/")
 	public String home() {
-		return "index";
+		return "redirect:/login/p";
 	}
 	
 	
-//	@RequestMapping("/test")  
-//	public String home2() {
-//		return "index";
-//	}
+	@RequestMapping("/alter")
+	public String admin() {
+		return "home";
+	}
 	
+	@RequestMapping("/login/p")
+	public String secure() {
+		return "home";
+	}
 	
+	@RequestMapping("/login")  
+	public String home2() {
+		return "login";
+	}	
+	
+	@RequestMapping("/create_account")  
+	public String create_account(@RequestParam Map<String,String> reqPar) {
+
+		String firstname = reqPar.get("fn");
+		String lastname = reqPar.get("ln");
+		String username = reqPar.get("un");
+		String password = reqPar.get("pw");
+		String city = reqPar.get("ct");
+		String state = reqPar.get("st");
+		String phone = reqPar.get("ph");
+		String email = reqPar.get("em");
+
+		System.out.println(" Registered User "+firstname+" , "+ lastname+" , "+ username+" , "+ password+" , "+ city+" , "+ state+" , "+ phone+" , "+ email);
+		
+		
+		
+		return "login";
+	}	
 }
